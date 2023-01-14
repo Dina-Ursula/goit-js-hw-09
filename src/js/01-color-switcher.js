a358f495eafa3a26febd8b291695d1dc08b351ae
Dina-Ursula <dinatyan26@gmail.com>
2023-01-14 23:31:12 +0200

    Added 01-color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+let interval;
+
+startButton.addEventListener('click', evt => {
+  evt.preventDefault();
+
+  startButton.disabled = true;
+  stopButton.disabled = false;
+
+  interval = setInterval(() => {
+    body.style.background = getRandomHexColor();
+  }, 1000);
+});
+
+stopButton.addEventListener('click', evt => {
+  evt.preventDefault();
+
+  startButton.disabled = false;
+  stopButton.disabled = true;
+
+  clearInterval(interval);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+stopButton.disabled = true;
