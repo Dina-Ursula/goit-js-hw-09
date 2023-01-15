e02847a1ce6676be9d947d32036074d67b66720e
Dina-Ursula <dinatyan26@gmail.com>
2023-01-15 19:33:54 +0200

    Added 03-promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+const form = document.querySelector('form');
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+
+  const formElements = evt.currentTarget.elements;
+  let delay = +formElements.delay.value;
+  const step = +formElements.step.value;
+  const amount = +formElements.amount.value;
+
+  for (let i = 0; i < amount; i += 1) {
+    createPromise(i + 1, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += step;
+  }
+});
